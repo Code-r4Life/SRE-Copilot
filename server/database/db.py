4e37f176fbe59752d83c34664f85edad97ccae66
Code-r4Life <shinjansaha00@gmail.com>
2026-05-25 10:57:24 +0530

    made env based changes and Procfile

--- server/database/db.py
+++ server/database/db.py
@@ -8,12 +8,14 @@ DB_HOST     = os.getenv("DB_HOST", "localhost")
 DB_USER     = os.getenv("DB_USER", "root")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME     = os.getenv("DB_NAME", "api_monitoring")
+DB_PORT     = int(os.getenv("DB_PORT", 3306))
 
 # CONNECT TO MYSQL (no database selected yet)
 db = mysql.connector.connect(
     host=DB_HOST,
     user=DB_USER,
-    password=DB_PASSWORD
+    password=DB_PASSWORD,
+    port=DB_PORT
 )
 cursor = db.cursor(dictionary=True)
 
@@ -45,7 +47,8 @@ def create_connection():
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
-        database=DB_NAME
+        database=DB_NAME,
+        port=DB_PORT
     )
     cursor = db.cursor(dictionary=True)
     return db, cursor
